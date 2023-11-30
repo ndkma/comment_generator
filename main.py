2da1d9ee1f71d1782f06fba5217cb5f6661d21bb
ndkma <nicowork13@gmail.com>
2023-12-01 01:06:39 +0800

    Started adding GUI; name field and subject scales

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import together
+from tkinter import *
 
-together.api_key = "<<YOUR API KEY HERE>>"
+together.api_key = "YOUR API KEY HERE"
 
 # Provide context for generating responses
 context_prompt = (
@@ -27,13 +28,13 @@ for user_msg in specific_prompt:
 
 output = together.Complete.create(
     prompt,
-    model="togethercomputer/llama-2-13b-chat",
-    max_tokens=70,
-    temperature=0.6,
-    top_k=90,
-    top_p=0.8,
-    repetition_penalty=1.1,
-    stop=['</s>']
+    model = "togethercomputer/llama-2-13b-chat",
+    max_tokens = 70,
+    temperature = 0.6,
+    top_k = 90,
+    top_p = 0.8,
+    repetition_penalty = 1.1,
+    stop = ['</s>']
 )
 
 complete_output = output['output']['choices'][0]['text']
@@ -44,3 +45,30 @@ short_output = complete_output.split(":")[1]
 
 # Needs to be output somewhere copy-able ===================================================
 print(short_output)
+
+# Create base GUI
+master = Tk()
+master.title("Report Card Comment Generator")
+master.geometry("1200x600")
+
+# Create student name input field
+student_name_field = Entry(master, font = 40).place(x = 200, y = 25)
+
+# Create static labels
+Label(master, text = "Student Name:", font=40).place(x = 25, y = 25)
+Label(master, text = "Writing Score:", font=40).place(x = 25, y = 125)
+Label(master, text = "Speaking Score:", font=40).place(x = 25, y = 225)
+Label(master, text = "Listening Score:", font=40).place(x = 25, y = 325)
+Label(master, text = "Reading Score:", font=40).place(x = 25, y = 425)
+
+# Create scales for scoring
+writing_scale = Scale(master, from_ = 0, to = 10, orient = HORIZONTAL, length = 218).place(x = 200, y = 112)
+speaking_scale = Scale(master, from_ = 0, to = 10, orient = HORIZONTAL, length = 218).place(x = 200, y = 212)
+listening_scale = Scale(master, from_ = 0, to = 10, orient = HORIZONTAL, length = 218).place(x = 200, y = 312)
+reading_scale = Scale(master, from_ = 0, to = 10, orient = HORIZONTAL, length = 218).place(x = 200, y = 412)
+
+# Create set score buttons
+
+
+
+master.mainloop()
